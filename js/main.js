0de4e5f7cfd5376b4efddbb44481fee30c256ab1
royweb <xytank@gmail.com>
2019-10-29 11:45:42 -0400

    Set up main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("It work");
+const choices = document.querySelectorAll('.choice');
+const score =  document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.getElementById('modal');
+
+const scoreBoard = {
+    player: 0,
+    cpu: 0
+}
+
+function play(e){
+    console.log(e.target.id);
+}
+
+// add event listeners to all choices
+choices.forEach(choice => choice.addEventListener('click', play));
