d75abaa05a044493a102e8ca68c16ff97bc4170d
royweb <xytank@gmail.com>
2019-11-02 13:29:34 -0400

    Add a function that adds a zero to the hour or second

--- js/time.js
+++ js/time.js
@@ -16,10 +16,15 @@ function showTime(){
     hour = hour % 12 || 12;
 
     // display the time
-    time.innerHTML = `${hour}<span>:</span>${minute}<span>:</span>${second} ${midday}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(minute)}<span>:</span>${addZero(second)} ${midday}`;
 
     // call the function every 1 second
     setTimeout(showTime, 1000);
+}
+
+// add zero
+function addZero(number){
+    return (parseInt(number, 10) < 10 ? '0' : '') + number;
 }
 
 // run the function
